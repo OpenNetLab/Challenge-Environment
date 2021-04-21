31899fcd954de45e4eed78b37d8421a2658339cf
Azson <240326315@qq.com>
2021-04-21 01:26:21 +0000

    support to evaluate network

--- metrics/eval.py
+++ metrics/eval.py
@@ -6,22 +6,24 @@ sys.path.append(os.getcwd())
 
 from metrics.eval_video import VideoEvaluation, init_video_argparse, fill_video_args, get_video_score
 from metrics.eval_audio import AudioEvaluation, init_audio_argparse, fill_audio_args, get_audio_score
+from metrics.eval_network import NetworkEvaluation, init_network_argparse, fill_network_args, get_network_score
 
 
 description = \
 '''
 Quickly Start:
-python3 eval.py --src_video {src_video} --dst_video {dst_video}  --dst_audio {dst_audio} --dnsmos_uri {dnsmos_uri} --dnsmos_key {dnsmos_key}
+python3 eval.py --src_video {src_video} --dst_video {dst_video}  \
+                --dst_audio {dst_audio} --dnsmos_uri {dnsmos_uri} --dnsmos_key {dnsmos_key}
+                --dst_network_log {dst_network_log} --output {outout}
 '''
 
 def init_argparse():
     video_parser = init_video_argparse()
     audio_parser = init_audio_argparse()
-    parser = argparse.ArgumentParser(description=description, parents=[video_parser, audio_parser], conflict_handler='resolve')
+    network_parser = init_network_argparse()
+    parser = argparse.ArgumentParser(description=description, parents=[video_parser, audio_parser, network_parser], conflict_handler='resolve')
     parser.add_argument("--method", type=str, default="simple", help="the method to evaluation")
     parser.add_argument("--output", type=str, default=None, help="the path of output file")
-    # for network evaluation
-    parser.add_argument("--network_dir", type=str, default=None, help="the directory of network files")
 
     args = parser.parse_args()
 
@@ -38,8 +40,14 @@ def fill_args(args):
     ret = {}
     video_ret = fill_video_args(args)
     audio_ret = fill_audio_args(args)
-    ret.update(video_ret)
-    ret.update(audio_ret)
+    network_ret = fill_network_args(args)
+
+    if (video_ret):
+        ret.update(video_ret)
+    if (audio_ret):
+        ret.update(audio_ret)
+    if (network_ret):
+        ret.update(network_ret)
 
     return ret
 
@@ -53,7 +61,8 @@ if __name__ == "__main__":
     if (args.method == "simple"):
         out_dict["video"] = get_video_score(args, eval_args)
         out_dict["audio"] = get_audio_score(args, eval_args)
-        
+        out_dict["network"] = get_network_score(args, eval_args)
+
     print(json.loads(json.dumps(out_dict)))
     with open(args.output, 'w') as f:
         f.write(json.dumps(out_dict))
--- tests/test_eval.py
+++ tests/test_eval.py
@@ -3,36 +3,43 @@
 import os, json, pytest
 
 
-def get_video_vmaf(src_video, dst_video, audio_path, dnsmos_uri, dnsmos_key, output):
+def get_score(src_video, dst_video, audio_path, dnsmos_uri, dnsmos_key, dst_network_log, output):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = cur_dir + "/../metrics/eval.py"
     cmd = ["python3", file_path, "--method simple", "--src_video", src_video, "--dst_video", dst_video, \
-                                 "--dnsmos_uri", dnsmos_uri, "--dnsmos_key", dnsmos_key, "--dst_audio", audio_path, "--output", output]
+                                 "--dnsmos_uri", dnsmos_uri, "--dnsmos_key", dnsmos_key, "--dst_audio", audio_path, \
+                                 "--dst_network_log", dst_network_log, "--output", output]
     os.system(' '.join(cmd))
     with open(output, 'r') as f:
         data = json.loads(f.read())
     assert "video" in data
+    assert "audio" in data
+    assert "network" in data
 
 
-def get_yuv_video_vmaf(src_video, dst_video, video_size, pixel_format, bitdepth, audio_path, dnsmos_uri, dnsmos_key, output):
+def get_yuv_score(src_video, dst_video, video_size, pixel_format, bitdepth, audio_path, dnsmos_uri, dnsmos_key, dst_network_log, output):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = cur_dir + "/../metrics/eval.py"
     cmd = ["python3", file_path, "--method simple", "--src_video", src_video, "--dst_video", dst_video, \
                                  "--video_size", video_size, "--pixel_format", pixel_format, "--bitdepth", bitdepth, \
-                                 "--dnsmos_uri", dnsmos_uri, "--dnsmos_key", dnsmos_key, "--dst_audio", audio_path, "--output", output]
+                                 "--dnsmos_uri", dnsmos_uri, "--dnsmos_key", dnsmos_key, "--dst_audio", audio_path, \
+                                 "--dst_network_log", dst_network_log, "--output", output]
     os.system(' '.join(cmd))
     with open(output, 'r') as f:
         data = json.loads(f.read())
     assert "video" in data
+    assert "audio" in data
+    assert "network" in data
 
 
-def test_y4m_vmaf(dnsmos_uri, dnsmos_key):
+def test_y4m_score(dnsmos_uri, dnsmos_key):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     output = cur_dir + "/output.json"
     src_video = cur_dir + "/data/horse.y4m"
     dst_video = cur_dir + "/data/horse.y4m"
     audio_path = cur_dir + "/data/test.wav"
-    get_video_vmaf(src_video, dst_video, audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, output=output)
+    dst_network_log = cur_dir + "/data/webrtc.out"
+    get_score(src_video, dst_video, audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, dst_network_log=dst_network_log, output=output)
 
 
 def test_yuv_vmaf(dnsmos_uri, dnsmos_key):
@@ -41,14 +48,16 @@ def test_yuv_vmaf(dnsmos_uri, dnsmos_key):
     src_video = cur_dir + "/data/horse.yuv"
     dst_video = cur_dir + "/data/horse.y4m"
     audio_path = cur_dir + "/data/test.wav"
-    get_video_vmaf(dst_video, src_video, audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, output=output)
-    get_video_vmaf(src_video, dst_video, audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, output=output)
+    dst_network_log = cur_dir + "/data/webrtc.out"
+    get_score(dst_video, src_video, audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, dst_network_log=dst_network_log, output=output)
+    get_score(src_video, dst_video, audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, dst_network_log=dst_network_log, output=output)
 
-def test_yuv_input_vmaf(dnsmos_uri, dnsmos_key):
+def test_yuv_input_score(dnsmos_uri, dnsmos_key):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     output = cur_dir + "/output.json"
     src_video = cur_dir + "/data/horse.yuv"
     dst_video = cur_dir + "/data/horse.yuv"
     audio_path = cur_dir + "/data/test.wav"
-    get_yuv_video_vmaf(dst_video, src_video, video_size="320x240", pixel_format="420", bitdepth="8", \
-                        audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, output=output)
+    dst_network_log = cur_dir + "/data/webrtc.out"
+    get_yuv_score(dst_video, src_video, video_size="320x240", pixel_format="420", bitdepth="8", \
+                        audio_path=audio_path, dnsmos_uri=dnsmos_uri, dnsmos_key=dnsmos_key, dst_network_log=dst_network_log, output=output)
