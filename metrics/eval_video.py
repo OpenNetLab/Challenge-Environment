d8385ffbb833487405d628eba7a9728c4d491275
Azson <240326315@qq.com>
2021-04-22 02:08:07 +0000

    reconstruct code 1. Make the class of EvalMethod to evaluate video; 2. Make the class of AlignMethod to align video; 3. Make the class of VideoInfo to save video information; 4. Use Regex to get video information from ffprobe log; 5. rename test data of horse to test; 6. Modify the argparser like add required and choices; 7. Use subprocess to call shell and get out fron specify PIPE; 8. Use template to save tmp files, like ffprobe log and new type of video; 9. Chmod 774 to files that contain declare of python3 in top;

--- metrics/eval_video.py
+++ metrics/eval_video.py
@@ -1,193 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os, sys, argparse, json, shutil
+
+import os, sys, argparse, json, subprocess
+from tempfile import NamedTemporaryFile
+from utils.video_info import VideoInfo
+from utils.eval_method import EvalMethodVmaf, EvalMethod
+from utils.align_method import AlignMethodFfmpeg, AlignMethod
 
 
 description = \
 '''
-Quickly Start:
-python3 eval_video.py --src_video {src_video} --dst_video {dst_video} --output {output_path}
+This file provide multi method to evaluate video quality based on class of EvalMethod. 
+For example, the class of EvalMethodVmaf inherit from EvalMethod can evaluate video quality by using vmaf tools.
 '''
 
 
-class VideoEvaluation():
-    def __init__(self, eval_method="vmaf", eval_args=None):
+class VideoEvaluation(object):
+    def __init__(self, eval_method : EvalMethod, align_method : AlignMethod, args):
         self.eval_method = eval_method
-        self.eval_args = self.fill_default_args()
-        if eval_args:
-            self.eval_args.update(eval_args)
-
-    def fill_default_args(self):
-        eval_args = dict()
-        if (self.eval_method == "vmaf"):
-            eval_args["dst_video_type"] = ["y4m"]
-            eval_args["video_out"] = "tmp/vmaf_out.xml"
-        return eval_args
-
-    def check_valid_of_path(self, path):
-        return os.path.exists(path)
-
-    def check_video_type(self, path):
-        if ("dst_video_type" not in self.eval_args):
-            return True
-        return path.split('.')[-1] in self.eval_args["dst_video_type"]
-
-    ### ffprobe orders below ###
-    def get_video_info_json(self, video_path):
-        ffprobe_info = self.get_video_info_by_ffprobe(path)
-        video_fps = self.get_video_fps_by_ffprobe(video_path, ffprobe_log=ffprobe_info)
-        video_dur_time = self.get_video_dur_time_by_ffprobe(video_path, ffprobe_log=ffprobe_info)
-        video_size = self.get_video_size_by_ffprobe(video_path, ffprobe_log=ffprobe_info)
-
-        ret = {}
-        ret["video_fps"] = video_fps
-        ret["video_dur_time"] = video_dur_time
-        ret["video_size"] = video_size
-
-        return ret
-
-    def get_video_info_by_ffprobe(self, path, output=None):
-        if not output:
-            output = "tmp/ffprobe.log"
-        cmd = ["ffprobe -show_format", path]
-        if "video_size" in self.eval_args and self.eval_args["video_size"]:
-            cmd.extend(["-video_size %sx%s" % (self.eval_args["video_size"][0], self.eval_args["video_size"][0][1])])
-        cmd.extend([">", output, "2>&1"])
-        os.system(' '.join(cmd))
-
-        return output
-
-    def get_video_fps_by_ffprobe(self, path, ffprobe_log=None):
-        ffprobe_log = ffprobe_log if ffprobe_log else self.get_video_info_by_ffprobe(path)
-        with open(ffprobe_log, 'r') as f:
-            out = list(filter(lambda x : "Stream" in x, f.readlines()))
-
-        if not len(out):
-            # raise ValueError("Can not get the fps from %s by ffprobe." % (path))
-            return None
-        fps = list(filter(lambda x : "fps" in x, out[0].split(',')))[0].strip().split(' ')[0]
-        return fps
-
-    def get_video_dur_time_by_ffprobe(self, path, ffprobe_log=None):
-        ffprobe_log = ffprobe_log if ffprobe_log else self.get_video_info_by_ffprobe(path)
-        with open(ffprobe_log, 'r') as f:
-            out = list(filter(lambda x : "Duration" in x, f.readlines()))
-
-        if not len(out):
-            # raise ValueError("Can not get the during time from %s by ffprobe." % (path))
-            return None
-        dur_time_stamp = list(filter(lambda x : "Duration" in x, out[0].split(',')))[0].strip().split(' ')[1].split(":")
-        dur_time = float(dur_time_stamp[0])*3600 + float(dur_time_stamp[1])*60 + float(dur_time_stamp[2])
-        return dur_time
-
-    def get_video_size_by_ffprobe(self, path, ffprobe_log=None):
-        ffprobe_log = ffprobe_log if ffprobe_log else self.get_video_info_by_ffprobe(path)
-        with open(ffprobe_log, 'r') as f:
-            out = list(filter(lambda x : "fps" in x, f.readlines()))
-
-        if not len(out):
-            # raise ValueError("Can not get the size from %s by ffprobe." % (path))
-            return None
-        video_size = list(filter(lambda x : "x" in x, out[0].split(',')))[1].strip().split('x')
-        return video_size
+        self.align_method = align_method
+        self.args = args
 
     ### ffmpeg orders below ###
 
-    def change_video_type(self, path, new_type, size=None):
-        output = "tmp/new_" + path.split('/')[-1].split('.')[0] + '.%s' % new_type
-        print(output)
-        # output = ''.join(path.split('.')[:-1]) + "." + new_type
+    def change_video_type(self, video_info : VideoInfo, new_type):
+        output = NamedTemporaryFile('w+t', suffix=".%s" % (new_type))
         cmd = ["ffmpeg"]
-        if (not size):
-            if ("video_size" in self.eval_args):
-                size = self.eval_args["video_size"]
-        if (size):
-            if (type(size) == str):
-                size = size.replace('X', 'x').split('x')
-            if (type(size[0]) != str):
-                size = list(map(lambda x : str(x), size))
-            cmd.append("-video_size %sx%s" % (size[0], size[1]))
-        cmd.extend(["-i", path])
-        cmd.append(output)
-        os.system(' '.join(cmd))
+        if video_info.format_name == "rawvideo":
+            cmd.extend(["-video_size", video_info.video_size])
+        cmd.extend(["-i", video_info.video_path, "-y"])
+        if new_type == "yuv":
+            cmd.extend(["-video_size", video_info.video_size])
+        cmd.append(output.name)
+        subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+
         return output
-
-    def change_video_fps_by_ffmepg(self, path, fps, size=None, output=None):
-        if not output:
-            preffix, suffix = path.split('.')[:-1], path.split('.')[-1]
-            output = ''.join(preffix) + '_%sfps.%s' % (fps, suffix)
-        cmd = ["ffmpeg -i", path, "-r", str(fps)]
-        if size and len(size) == 2:
-            cmd.extend(["-s %sx%s" % (size[0], size[1])])
-        cmd.append(output)
-        os.system(' '.join(cmd))
-        return output
-
-    def frame_align_normal(self, src_video_path, dst_video_path):
-        # Caculate the frame info by using ffprobe
-        src_video_info = self.get_video_info_json(src_video_path)
-        dst_video_info = self.get_video_info_json(dst_video_info)
-
-        if "video_fps" not in self.eval_args:
-            self.eval_args["video_fps"] = src_video_info["video_fps"] if src_video_info["video_fps"] else dst_video_info["video_fps"]
-        if "video_dur_time" not in self.eval_args:
-            self.eval_args["video_dur_time"] = src_video_info["video_dur_time"] if src_video_info["video_dur_time"] else dst_video_info["video_dur_time"]
-        if "video_size" not in self.eval_args:
-            self.eval_args["video_size"] = src_video_info["video_size"] if src_video_info["video_size"] else dst_video_info["video_size"]
-        
-        # Frame alignment
-        if (abs(src_video_dur_time*src_video_fps - dst_video_dur_time*dst_video_fps) >= 0.000001):
-            new_fps = dst_video_fps * dst_video_dur_time / src_video_dur_time
-            new_src_video_path = self.change_video_fps_by_ffmepg(src_video_path, new_fps, self.eval_args["video_size"])
-        else:
-            new_src_video_path = dst_video_path
-
-        return new_src_video_path
-
-    def eval_by_vmaf(self, src_video_path, dst_video_path, size=None):            
-        cmd = ["vmaf", "--reference", src_video_path, "--distorted", dst_video_path]
-        if src_video_path.split('.')[-1] == "yuv":
-            cmd.extend(["--width", size[0], "--height", size[1], \
-                "--pixel_format", self.eval_args["pixel_format", "--bitdepth", self.eval_args["bitdepth"]]])
-        cmd.extend(["--output", self.eval_args["video_out"]])
-        os.system(' '.join(cmd))
-        with open(self.eval_args["video_out"], 'r') as f:
-            vmaf_score_line = list(filter(lambda x : "metric" in x and "vmaf" in x, f.readlines()))
-            vmaf_score = list(filter(lambda x : "mean" in x, vmaf_score_line[0].split(' ')))[0].split('=')[-1][1:-1]
-
-        return vmaf_score
 
     def eval(self, src_video_path, dst_video_path):
-        # Check the validity of the path
-        if not self.check_valid_of_path(src_video_path):
-            raise ValueError("The path of %s is invalid." % (src_video_path))
-        if not self.check_valid_of_path(dst_video_path):
-            raise ValueError("The path of %s is invalid." % (dst_video_path))
+        # get the correspond VideoInfo according to path
+        src_video_info = VideoInfo(src_video_path, self.args.video_size)
+        dst_video_info = VideoInfo(dst_video_path, self.args.video_size)
+
         # init video size
-        if not self.eval_args["video_size"]:
-            self.eval_args["video_size"] =  self.get_video_size_by_ffprobe(dst_video_path)
-            if not self.eval_args["video_size"]:
-                self.eval_args["video_size"] =  self.get_video_size_by_ffprobe(src_video_path)
-        # Check video type
-        if not self.check_video_type(src_video_path):
-            src_video_path = self.change_video_type(src_video_path, self.eval_args["dst_video_type"][0])
-        if not self.check_video_type(dst_video_path):
-            dst_video_path = self.change_video_type(dst_video_path, self.eval_args["dst_video_type"][0])
+        if not src_video_info.video_size and not dst_video_info.video_size:
+            raise ValueError("Please specify the arguments --video_size")
+ 
+        if not dst_video_info.video_size:
+            dst_video_info.update_video_size(src_video_info.video_size)
+        elif not src_video_info.video_size:
+            src_video_info.update_video_size(dst_video_info.video_size)
 
-        if ("frame_align" in self.eval_args and self.eval_args["frame_align"] == "normal"):
-            src_video_path = self.frame_align_normal(src_video_path, dst_video_path)
+        # update video info
+        src_video_info.parse_video_by_ffprobe(video_size=src_video_info.video_size)
+        dst_video_info.parse_video_by_ffprobe(video_size=dst_video_info.video_size)
+        video_size = src_video_info.video_size
 
-        ret = 0
+        # check video type
+        if src_video_info.format_name not in self.eval_method.support_type:
+            fp_new_src_video = self.change_video_type(src_video_info, self.eval_method.support_type_abbreviation[0])
+            src_video_info = VideoInfo(fp_new_src_video.name)
+        if dst_video_info.format_name not in self.eval_method.support_type:
+            fp_new_dst_video = self.change_video_type(dst_video_info, self.eval_method.support_type_abbreviation[0])
+            dst_video_info = VideoInfo(fp_new_dst_video.name)
+        # keep same video type
+        if src_video_info.format_abbreviation != "y4m":
+            fp_new_src_video = self.change_video_type(src_video_info, "y4m")
+            src_video_info = VideoInfo(fp_new_src_video.name, video_size=video_size)
+        if dst_video_info.format_abbreviation != "y4m":
+            fp_new_dst_video = self.change_video_type(dst_video_info, "y4m")
+            dst_video_info = VideoInfo(fp_new_dst_video.name, video_size=video_size)
+
+        # use args or align_method?
+        if self.args.frame_align == "ffmpeg":
+            if not src_video_info.fps and not dst_video_info.fps:
+                raise ValueError("Can't get fps from video")
+            if src_video_info.fps:
+                fp_new_video = self.align_method.frame_align(dst_video_info, src_video_info)
+                dst_video_info = VideoInfo(fp_new_video.name) if fp_new_video else dst_video_info
+            else:
+                fp_new_video = self.align_method.frame_align(src_video_info, dst_video_info)
+                src_video_info = VideoInfo(fp_new_video.name) if fp_new_video else src_video_info
+
         # Calculate video quality
-        if self.eval_method == "vmaf":
-            ret = self.eval_by_vmaf(src_video_path, dst_video_path, self.eval_args["video_size"]  )
-
+        ret = self.eval_method.eval(src_video_info, dst_video_info)
+        
         return ret
 
 
-def get_video_score(args, eval_args):
-    video_eval_tool = VideoEvaluation(args.video_eval_method, eval_args=eval_args)
+def get_video_score(eval_method : EvalMethod, align_method : AlignMethod, args):
+    video_eval_tool = VideoEvaluation(eval_method, align_method, args)
     video_out = video_eval_tool.eval(args.src_video, args.dst_video)
+    
     return video_out
 
 
@@ -195,44 +98,35 @@ def init_video_argparse():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("--output", type=str, default=None, help="the path of output file")
     # for video evaluation
-    parser.add_argument("--video_eval_method", type=str, default="vmaf", help="the method to evaluate video, like vmaf")
-    parser.add_argument("--src_video", type=str, default=None, help="the path of source video")
-    parser.add_argument("--dst_video", type=str, default=None, help="the path of destination video")
-    parser.add_argument("--frame_align", type=str, default="ffmpeg", help="how to do frame alignment")
+    parser.add_argument("--video_eval_method", type=str, default="vmaf", choices=["None", "vmaf"], help="the method to evaluate video, like vmaf")
+    parser.add_argument("--src_video", type=str, required=True, default=None, help="the path of source video")
+    parser.add_argument("--dst_video", type=str, required=True, default=None, help="the path of destination video")
+    parser.add_argument("--frame_align", type=str, default="ffmpeg", choices=["None", "ffmpeg"], help="how to do frame alignment")
     # required by the video format of yuv raw video
-    parser.add_argument("--video_size", type=str, default=None, help="the size of video, like 1920x1080.Required by the video format of yuv")
-    parser.add_argument("--pixel_format", type=str, default=None, help="pixel format (420/422/444)")
-    parser.add_argument("--bitdepth", type=str, default=None, help="bitdepth (8/10/12)")
+    parser.add_argument("--video_size", type=str, default=None, help="the size of video, like 1920x1080. Required by the video format of yuv")
+    parser.add_argument("--pixel_format", type=str, default=None, choices=["420", "422", "444"], help="pixel format (420/422/444)")
+    parser.add_argument("--bitdepth", type=str, default=None, choices=["8", "10", "12"], help="bitdepth (8/10/12)")
 
     return parser
-
-
-def init_tmp_dir():
-    if os.path.exists("tmp"):
-        shutil.rmtree("tmp")
-    os.mkdir("tmp")
-
-
-def fill_video_args(args):
-    ret = dict()
-    ret["frame_align"] = args.frame_align
-    ret["output"] = args.output
-    ret["video_size"] = args.video_size
-    ret["pixel_format"] = args.pixel_format
-    ret["bitdepth"] = args.bitdepth
-
-    return ret if len(ret) else None
 
 
 if __name__ == "__main__":
     parser = init_video_argparse()
     args = parser.parse_args()
-    eval_args = fill_video_args(args)
-    init_tmp_dir()
     out_dict = {}
+    eval_method = None
+    align_method = None
+
     if (args.video_eval_method == "vmaf"):
-        out_dict["video"] = get_video_score(args, eval_args)
+        eval_method = EvalMethodVmaf()
+    
+    if (args.frame_align == "ffmpeg"):
+        align_method = AlignMethodFfmpeg()
+
+    out_dict["video"] = get_video_score(eval_method, align_method, args)
         
-    print(json.loads(json.dumps(out_dict)))
-    with open(args.output, 'w') as f:
-        f.write(json.dumps(out_dict))
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write(json.dumps(out_dict))
+    else:
+        print(json.dumps(out_dict))
--- tests/test_eval_video.py
+++ tests/test_eval_video.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os, json, pytest
+
+import os, json, pytest, subprocess
 
 
-def get_video_vmaf(src_video, dst_video, output):
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = cur_dir + "/../metrics/eval_video.py"
-    cmd = ["python3", file_path, "--video_eval_method vmaf", "--src_video", src_video, "--dst_video", dst_video, "--output", output]
-    os.system(' '.join(cmd))
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = cur_dir + "/../metrics/eval_video.py"
+output = cur_dir + "/output.json"
+
+
+def check_video_vmaf(src_video, dst_video, output):
+    cmd = ["python3", file_path, "--video_eval_method", "vmaf", "--src_video", src_video, "--dst_video", dst_video]
+    cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+    assert "video" in json.loads(cmd_result.stdout)
+    
+    # check output file
+    cmd.extend(["--output", output])
     with open(output, 'r') as f:
         data = json.loads(f.read())
     assert "video" in data
 
 
-def get_yuv_video_vmaf(src_video, dst_video, video_size, pixel_format, bitdepth, output):
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = cur_dir + "/../metrics/eval_video.py"
-    cmd = ["python3", file_path, "--video_eval_method vmaf", "--src_video", src_video, "--dst_video", dst_video, \
-                                 "--video_size", video_size, "--pixel_format", pixel_format, "--bitdepth", bitdepth, "--output", output]
-    os.system(' '.join(cmd))
+def check_yuv_video_vmaf(src_video, dst_video, video_size, pixel_format, bitdepth, output):
+    cmd = ["python3", file_path, "--video_eval_method", "vmaf", "--src_video", src_video, "--dst_video", dst_video, \
+                                 "--video_size", video_size, "--pixel_format", pixel_format, "--bitdepth", bitdepth]
+    cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+    assert "video" in json.loads(cmd_result.stdout)
+    
+    # check output file
+    cmd.extend(["--output", output])
     with open(output, 'r') as f:
         data = json.loads(f.read())
     assert "video" in data
 
 
-def test_y4m_vmaf():
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    output = cur_dir + "/output.json"
-    src_video = cur_dir + "/data/horse.y4m"
-    dst_video = cur_dir + "/data/horse.y4m"
-    get_video_vmaf(src_video, dst_video, output)
+def test_y4m_y4m_compare():
+    src_video = cur_dir + "/data/test.y4m"
+    dst_video = cur_dir + "/data/test.y4m"
+    check_video_vmaf(src_video, dst_video, output)
 
 
-def test_yuv_vmaf():
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    output = cur_dir + "/output.json"
-    src_video = cur_dir + "/data/horse.yuv"
-    dst_video = cur_dir + "/data/horse.y4m"
-    get_video_vmaf(dst_video, src_video, output)
-    get_video_vmaf(src_video, dst_video, output)
+def test_y4m_yuv_compare():
+    src_video = cur_dir + "/data/test.yuv"
+    dst_video = cur_dir + "/data/test.y4m"
+    check_video_vmaf(dst_video, src_video, output)
+    check_video_vmaf(src_video, dst_video, output)
 
-def test_yuv_input_vmaf():
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    output = cur_dir + "/output.json"
-    src_video = cur_dir + "/data/horse.yuv"
-    dst_video = cur_dir + "/data/horse.yuv"
-    get_yuv_video_vmaf(dst_video, src_video, video_size="320x240", pixel_format="420", bitdepth="8", output=output)
+def test_yuv_yuv_compare():
+    src_video = cur_dir + "/data/test.yuv"
+    dst_video = cur_dir + "/data/test.yuv"
+    check_yuv_video_vmaf(dst_video, src_video, video_size="320x240", pixel_format="420", bitdepth="8", output=output)
