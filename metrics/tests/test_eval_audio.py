1c7829c01fdfe7c088665407cf3e75e4d56343f8
Azson <240326315@qq.com>
2021-04-22 09:14:40 +0000

    make some adaptation to azure in audio evaluation

--- metrics/tests/test_eval_audio.py
+++ metrics/tests/test_eval_audio.py
@@ -9,7 +9,7 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def check_audio_dnsmos(audio_path, dnsmos_uri, dnsmos_key):
-    file_path = cur_dir + "/../metrics/eval_audio.py"
+    file_path = cur_dir + "/../eval_audio.py"
     cmd = ["python3", file_path, "--dnsmos_uri", dnsmos_uri, "--dnsmos_key", dnsmos_key, "--dst_audio", audio_path]
     cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
 
