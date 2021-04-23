567bda330a60609cfa127c34bf049277c324087e
Azson <240326315@qq.com>
2021-04-23 15:41:07 +0800

    evaluate network quality (#7)
    * init first version to evaluate network
    
    * refactor the network evaluation related code and add tests in azure
    
    * move the method to related function
    
    * make some adaptation to azure in network evaluation
    
    * use commom test in azure pipelines and rename the test data
    
    * Use abstractmethod in EvalMethod and add descripttion for args
    
    * change the calculation of delay to the way in alphartc_record
    
    Co-authored-by: Ze Gan <ganze718@gmail.com>

---
+++ metrics/tests/test_network.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, json, subprocess
+from tempfile import NamedTemporaryFile
+
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def check_network_score(dst_network_log):
+    file_path = cur_dir + "/../eval_network.py"
+    cmd = ["python3", file_path, "--network_eval_method", "normal", "--dst_network_log", dst_network_log]
+    cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+    
+    assert "network" in json.loads(cmd_result.stdout)
+
+    # check output file
+    with NamedTemporaryFile('w+t') as output:
+        cmd.extend(["--output", output.name])
+        cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+        data = json.loads(output.read())
+        assert "network" in data
+
+
+def test_network_score():
+    dst_network_log = cur_dir + "/data/alphartc.log"
+    check_network_score(dst_network_log)
