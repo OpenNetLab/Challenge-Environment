2ef56a12bd3cf6c3620b59846bde6a7400c07bf6
Azson <240326315@qq.com>
2021-04-23 21:05:51 +0800

    Evaluate video (#3)
    * init first version
    
    * update Makefile
    
    * init chanllenge envirobment
    
    * add metrics dir
    
    * init to evaluate audio by dnsmos
    
    * delete the files that not used in test dir
    
    * init to evaluate video by vmaf
    
    * split eval_video.py from eval.py
    
    * output json instead of python dict
    
    * reconstruct code
    1. Make the class of EvalMethod to evaluate video;
    2. Make the class of AlignMethod to align video;
    3. Make the class of VideoInfo to save video information;
    4. Use Regex to get video information from ffprobe log;
    5. rename test data of horse to test;
    6. Modify the argparser like add required and choices;
    7. Use subprocess to call shell and get out fron specify PIPE;
    8. Use template to save tmp files, like ffprobe log and new type of video;
    9. Chmod 774 to files that contain declare of python3 in top;
    
    * rm horse data
    
    * add test in azure and support to specify the vmaf model path
    
    * move the method class to related functions
    
    * make some adaptation to azure in video evaluation
    
    * delete redundant scripts in azure_pipelines
    
    * use common test in azure pipelines
    
    * use abstraction in utils and chmod for files with no main function
    
    * rm hard code of vmaf model path
    
    * update dependency of vmaf

---
+++ metrics/tests/test_eval_video.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, json, pytest, subprocess
+from tempfile import NamedTemporaryFile
+
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = cur_dir + "/../eval_video.py"
+
+
+def check_video_vmaf(src_video, dst_video):
+    cmd = ["python3", file_path, "--video_eval_method", "vmaf", "--src_video", src_video, "--dst_video", dst_video]
+    cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+    assert "video" in json.loads(cmd_result.stdout)
+    
+    # check output file
+    with NamedTemporaryFile('w+t') as output:
+        cmd.extend(["--output", output.name])
+        cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+        data = json.loads(output.read())
+        assert "video" in data
+
+
+def check_yuv_video_vmaf(src_video, dst_video, video_size, pixel_format, bitdepth):
+    cmd = ["python3", file_path, "--video_eval_method", "vmaf", "--src_video", src_video, "--dst_video", dst_video, \
+                                 "--video_size", video_size, "--pixel_format", pixel_format, "--bitdepth", bitdepth]
+    cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+    assert "video" in json.loads(cmd_result.stdout)
+    
+    # check output file
+    with NamedTemporaryFile('w+t') as output:
+        cmd.extend(["--output", output.name])
+        cmd_result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+        data = json.loads(output.read())
+        assert "video" in data
+
+
+def test_y4m_y4m_compare():
+    src_video = cur_dir + "/data/test.y4m"
+    dst_video = cur_dir + "/data/test.y4m"
+    check_video_vmaf(src_video, dst_video)
+
+
+def test_y4m_yuv_compare():
+    src_video = cur_dir + "/data/test.yuv"
+    dst_video = cur_dir + "/data/test.y4m"
+    check_video_vmaf(dst_video, src_video)
+    check_video_vmaf(src_video, dst_video)
+
+def test_yuv_yuv_compare():
+    src_video = cur_dir + "/data/test.yuv"
+    dst_video = cur_dir + "/data/test.yuv"
+    check_yuv_video_vmaf(dst_video, src_video, video_size="320x240", pixel_format="420", bitdepth="8")
---
+++ metrics/utils/video_align_method.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+from tempfile import NamedTemporaryFile
+from utils.video_info import VideoInfo
+from abc import ABC, abstractmethod
+
+
+class VideoAlignMethod(ABC):
+    @abstractmethod
+    def __init__(self):
+        self.align_method_name = "base"
+
+    @abstractmethod
+    def frame_align(self, src_video_info : VideoInfo, dst_video_info : VideoInfo):
+        pass
+
+
+class VideoAlignMethodFfmpeg(VideoAlignMethod):
+    def __init__(self):
+        super(VideoAlignMethodFfmpeg, self).__init__()
+        self.align_method_name = "ffmpeg"
+
+    def change_video_fps_by_ffmepg(self, video_info : VideoInfo, fps : int):
+        output = NamedTemporaryFile('w+t', suffix=".%s" % (video_info.format_abbreviation))
+        cmd = ["ffmpeg", "-i", video_info.video_path, "-r", str(fps), "-y"]
+        if video_info.video_size:
+            cmd.extend(["-s", video_info.video_size])
+        cmd.append(output.name)
+        subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf8")
+        return output
+
+    def frame_align(self, src_video_info : VideoInfo, dst_video_info : VideoInfo):
+
+        fp_new_video = None
+        # Frame alignment
+        if not src_video_info.fps or \
+                abs(src_video_info.get_frame_count() - dst_video_info.get_frame_count()) >= 0.000001:
+            new_fps = dst_video_info.get_frame_count() / float(src_video_info.duration_sec)
+            fp_new_video = self.change_video_fps_by_ffmepg(src_video_info, new_fps)
+
+        return fp_new_video
