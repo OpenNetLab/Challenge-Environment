11c0e59db213cfebd3fa302a59d6b784ec1e897f
Azson <19505956+Azson@users.noreply.github.com>
2021-06-15 23:34:31 +0800

    Optimize network metrics (#18)
    * use the pencentile 95 of delay to cal network score
    
    * add the related test
    
    * rm the delay_effect_interval in network metrics
    
    * scale delay that greater to max_delay
    
    * add the recv rate score
    
    * add the loss rate score
    
    * use the dynamically recv rate
    
    * update the way to calculate recv_rate
    
    * update the way to calculate the recv_rate score
    
    * add the parameters of ground_recv_rate
    
    * set ground truth for recv_rate score

--- metrics/utils/net_eval_method.py
+++ metrics/utils/net_eval_method.py
@@ -17,10 +17,11 @@ class NetEvalMethod(ABC):
 
 
 class NetEvalMethodNormal(NetEvalMethod):
-    def __init__(self, delay_effect_interval=200):
+    def __init__(self, max_delay=400, ground_recv_rate=500):
         super(NetEvalMethodNormal, self).__init__()
         self.eval_name = "normal"
-        self.delay_effect_interval = delay_effect_interval
+        self.max_delay = max_delay
+        self.ground_recv_rate = ground_recv_rate
 
     def eval(self, dst_audio_info : NetInfo):
         net_data = dst_audio_info.net_data
@@ -36,30 +37,39 @@ class NetEvalMethodNormal(NetEvalMethod):
             if (ssrc not in ssrc_info):
                 ssrc_info[ssrc] = {
                     "time_delta" : -tmp_delay,
-                    "delay_list" : []
+                    "delay_list" : [],
+                    "received_nbytes" : 0,
+                    "start_recv_time" : item["packetInfo"]["arrivalTimeMs"],
+                    "avg_recv_rate" : 0
                 }
             if ssrc in self.last_seqNo:
                 loss_count += max(0, sequence_number - self.last_seqNo[ssrc] - 1)
             self.last_seqNo[ssrc] = sequence_number
                 
             ssrc_info[ssrc]["delay_list"].append(ssrc_info[ssrc]["time_delta"] + tmp_delay)
+            ssrc_info[ssrc]["received_nbytes"] += item["packetInfo"]["payloadSize"]
+            if item["packetInfo"]["arrivalTimeMs"] != ssrc_info[ssrc]["start_recv_time"]:
+                ssrc_info[ssrc]["avg_recv_rate"] = ssrc_info[ssrc]["received_nbytes"] / (item["packetInfo"]["arrivalTimeMs"] - ssrc_info[ssrc]["start_recv_time"])
+            
         # scale delay list
         for ssrc in ssrc_info:
             min_delay = min(ssrc_info[ssrc]["delay_list"])
-            # make offset if the first packet don't have the min delay
-            base_delay = 0 if min_delay >= 0 else -min_delay
-            mean_delay = np.mean(ssrc_info[ssrc]["delay_list"]) + base_delay
-            max_delay = mean_delay + self.delay_effect_interval
-            ssrc_info[ssrc]["scale_delay_list"] = [min(delay+base_delay, max_delay) / max_delay for delay in ssrc_info[ssrc]["delay_list"]]
+            ssrc_info[ssrc]["scale_delay_list"] = [min(self.max_delay, delay) for delay in ssrc_info[ssrc]["delay_list"]]
+            delay_pencentile_95 = np.percentile(ssrc_info[ssrc]["scale_delay_list"], 95)
+            ssrc_info[ssrc]["delay_score"] = (self.max_delay - delay_pencentile_95) / (self.max_delay - min_delay / 2)
         # delay score
-        avg_delay_score = np.mean([np.mean(ssrc_info[ssrc]["scale_delay_list"]) for ssrc in ssrc_info])
+        avg_delay_score = np.mean([np.mean(ssrc_info[ssrc]["delay_score"]) for ssrc in ssrc_info])
+
+        # receive rate score
+        recv_rate_list = [ssrc_info[ssrc]["avg_recv_rate"] for ssrc in ssrc_info if ssrc_info[ssrc]["avg_recv_rate"] > 0]
+        avg_recv_rate_score = min(1, np.mean(recv_rate_list) / self.ground_recv_rate)
 
         # higher loss rate, lower score
         avg_loss_rate = loss_count / (loss_count + len(net_data))
 
-        # calculate result score and the score composition can be evenly divided into two parts
-        avg_score = 50
-        network_score = avg_score * (1 - avg_delay_score) + \
-                            avg_score * (1 - avg_loss_rate)
+        # calculate result score
+        network_score = 100 * 0.2 * avg_delay_score + \
+                            100 * 0.2 * avg_recv_rate_score + \
+                            100 * 0.3 * (1 - avg_loss_rate)
 
         return network_score
