21a1fbfb4ba733662786a24b7918dab690a59ec1
wangyf2001 <392019123@qq.com>
2022-12-01 12:22:51 +0000

    eval.py

--- metrics/eval.py
+++ metrics/eval.py
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     out_dict["video"] = get_video_score(args)
     out_dict["network"] = get_network_score(args)
     # final_score = 0.2 * video + 0.1 * audio + (0.2 * delay_score + 0.2 * recv_rate_score + 0.3 * loss_score)
+    # We don't consider audio now. Give full score for the audio directly.
     out_dict["final_score"] = 0.2 * out_dict["video"] + out_dict["network"] + 10
-    #We don't consider audio now. Give 0.1 directly
     if args.output:
         with open(args.output, 'w') as f:
             f.write(json.dumps(out_dict))
